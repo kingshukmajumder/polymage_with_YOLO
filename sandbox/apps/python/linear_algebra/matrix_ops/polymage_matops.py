092c09b96cf2b07b86e1ac3880a2dbcdf7a50dcd
kumudhan <kumsbpips@gmail.com>
2016-06-20 16:49:25 +0530

    Initial checkin for common matrix operations. Still need to fix scalar mul and transpose (ref error)

---
+++ sandbox/apps/python/linear_algebra/matrix_ops/polymage_matops.py
@@ -0,0 +1,60 @@
+from __init__ import *
+
+import sys
+import subprocess
+import numpy as np
+from fractions import Fraction
+
+sys.path.insert(0, ROOT)
+
+from compiler import *
+from constructs import *
+
+def mat_ops(pipe_data):
+
+    R = Parameter(Int, "R")
+    C = Parameter(Int, "C")
+
+    pipe_data['R'] = R
+    pipe_data['C'] = C
+
+    x = Variable(UInt, 'x')
+    y = Variable(UInt, 'y')
+
+    row = Interval(Int, 0, R-1 )
+    col = Interval(Int, 0, C-1 )
+    
+    mat1 = Matrix(Float, "mat1", [R, C], [x, y])
+    mat2 = Matrix(Float, "mat3", [R, C], [x, y])
+
+    # Element-wise addition
+    add = Matrix(Float, "add", [R, C], [x, y])
+    add.defn = [mat2(x,y) + mat1(x,y)]
+    # return add
+
+    # Element-wise subtraction
+    sub = Matrix(Float, "sub", [R, C], [x, y])
+    sub.defn = [add(x,y) - mat2(x,y)]
+    # return sub
+
+    # Scalar multiplication
+    scalar_mul = Matrix(Float, "scalar_mul", [R, C], [x, y])
+    scalar_mul.defn = [sub(x,y) * 10.2]
+    # return scalar_mul
+
+    # Element-wise multiplication
+    mul = Matrix(Float, "mul", [R, C], [x, y])
+    mul.defn = [mat1(x,y) * sub(x,y)]
+    # return mul
+
+    # Transpose of a matrix
+    transpose = Matrix(Float, "transpose", [R, C], [x, y])
+    transpose.defn = [mat1(y, x)]
+    # return transpose
+
+    # Element-wise division
+    div = Matrix(Float, "div", [R, C], [x, y])
+    div.defn = [mul(x,y) / sub(x,y)]
+    return div
+
+
