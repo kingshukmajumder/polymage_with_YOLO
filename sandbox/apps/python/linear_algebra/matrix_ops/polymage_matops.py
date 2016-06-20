6f9095436808c669b32f478853adf51b2fcf6151
kumudhan <kumsbpips@gmail.com>
2016-06-20 19:33:39 +0530

    Fixed the issue with scalar multiplication and transpose

--- sandbox/apps/python/linear_algebra/matrix_ops/polymage_matops.py
+++ sandbox/apps/python/linear_algebra/matrix_ops/polymage_matops.py
@@ -21,26 +21,23 @@ def mat_ops(pipe_data):
     x = Variable(UInt, 'x')
     y = Variable(UInt, 'y')
 
-    row = Interval(Int, 0, R-1 )
-    col = Interval(Int, 0, C-1 )
-    
     mat1 = Matrix(Float, "mat1", [R, C], [x, y])
     mat2 = Matrix(Float, "mat3", [R, C], [x, y])
 
+    # Scalar multiplication
+    scalar_mul = Matrix(Float, "scalar_mul", [R, C], [x, y])
+    scalar_mul.defn = [mat1(x,y) * 10]
+    # return scalar_mul
+
     # Element-wise addition
     add = Matrix(Float, "add", [R, C], [x, y])
-    add.defn = [mat2(x,y) + mat1(x,y)]
+    add.defn = [scalar_mul(x,y) + mat2(x,y)]
     # return add
 
     # Element-wise subtraction
     sub = Matrix(Float, "sub", [R, C], [x, y])
     sub.defn = [add(x,y) - mat2(x,y)]
     # return sub
-
-    # Scalar multiplication
-    scalar_mul = Matrix(Float, "scalar_mul", [R, C], [x, y])
-    scalar_mul.defn = [sub(x,y) * 10.2]
-    # return scalar_mul
 
     # Element-wise multiplication
     mul = Matrix(Float, "mul", [R, C], [x, y])
@@ -49,12 +46,12 @@ def mat_ops(pipe_data):
 
     # Transpose of a matrix
     transpose = Matrix(Float, "transpose", [R, C], [x, y])
-    transpose.defn = [mat1(y, x)]
+    transpose.defn = [mul(y, x)]
     # return transpose
 
     # Element-wise division
     div = Matrix(Float, "div", [R, C], [x, y])
-    div.defn = [mul(x,y) / sub(x,y)]
+    div.defn = [transpose(x,y) / mat2(x,y)]
     return div
 
 
