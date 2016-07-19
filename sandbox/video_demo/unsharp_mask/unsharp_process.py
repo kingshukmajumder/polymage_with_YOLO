00e491b3dc67cec579b84ec213aa5983a0a56161
Krishna Kinnal <krishnakinnal@gmail.com>
2016-07-19 10:30:39 +0000

    Theano function definitions

--- sandbox/video_demo/unsharp_mask/unsharp_process.py
+++ sandbox/video_demo/unsharp_mask/unsharp_process.py
@@ -5,15 +5,23 @@ import sys
 from structs import *
 from PIL import Image, ImageFilter
 from numba_driver import unsharp_numba_driver
-from numba import jit
+from numba import *
+import theano.tensor as T
+from theano.tensor.nnet import conv2d
+import theano
 
 sys.path.insert(0, "../")
+
+NUMBA_OPT = 10
+NUMBA_LOOP_VECTORIZE = 1
+NUMBA_ENABLE_AVX = 1
 
 # unsharp mask parameters
 thresh = 0.001
 weight = 3
 
 # PIL version
+@jit("float32[:,:,:](uint8[:,:,:],none)",cache = True, forceobj=True, nogil = True)
 def unsharp_pil(frame, lib_func):
     im = Image.fromarray(frame)
     kernelx = (0,0,0,0,0,0,0,0,0,0,1,4,6,4,1,0,0,0,0,0,0,0,0,0,0)
@@ -25,7 +33,7 @@ def unsharp_pil(frame, lib_func):
     return res
 
 # OpenCV version
-@jit("float32[::](uint8[::], int64)", cache = True, nogil = True)
+@jit("float32[:,:,:](uint8[:,:,:], none)", cache = True, forceobj=True, nogil = True)
 def unsharp_cv(frame, lib_func):
     frame_f = np.float32(frame) / 255.0
     res = frame_f
@@ -36,6 +44,54 @@ def unsharp_cv(frame, lib_func):
     th, choose = threshold(absdiff(frame_f, blury), thresh, 1, THRESH_BINARY)
     choose = choose.astype(bool)
     np.copyto(res, sharpen, 'same_kind', choose)
+    return res
+
+def unsharp_theano(frame,lib_func):
+    frame = np.float32(frame) / 255.0
+    kernelx = np.asarray([[[[1,4,6,4,1]]]], dtype = 'float32') / 16
+    kernely = np.asarray([[[[1],[4],[6],[4],[1]]]], dtype = 'float32') / 16
+    frame = np.rollaxis(frame,2)
+
+    R = np.asarray([[frame[0]]], dtype='float32')
+    G = np.asarray([[frame[1]]], dtype='float32')
+    B = np.asarray([[frame[2]]], dtype='float32')
+
+    """Start of Theano definitions"""
+
+    #Blur function definition
+    t_img = T.ftensor4("t_img")
+    t_kernel = T.ftensor4("t_kernel")
+    blurred = conv2d(input = t_img, filters = t_kernel, border_mode = 'half')
+    blur = theano.function([t_img,t_kernel],blurred)
+
+    #Sharpen function definition
+    t_blurred = T.ftensor4("t_blurred")
+    t_weight = T.fscalar("t_weight")
+    sharpen = t_img * (1 + t_weight) - t_blurred * (t_weight)
+    sharp = theano.function([t_img,t_blurred,t_weight], sharpen)
+
+    #Masking function definition
+    t_sharpen = T.ftensor4("t_sharpen")
+    t_thresh = T.scalar("t_thresh")
+    t_mask = T.switch(T.lt(abs(t_img - t_blurred), t_thresh), t_img, t_sharpen)
+    masking = theano.function([t_img,t_blurred,t_sharpen,t_thresh], t_mask, mode=theano.Mode(linker='vm'))
+
+    """End of Theano definitions"""
+
+    Rblur = blur(blur(R,kernelx),kernely)
+    Gblur = blur(blur(G,kernelx),kernely)
+    Bblur = blur(blur(B,kernelx),kernely)
+
+    Rsharp = sharp(R, Rblur, weight)
+    Gsharp = sharp(G, Gblur, weight)
+    Bsharp = sharp(B, Bblur, weight)
+
+    Rfinal = masking(R,Rblur,Rsharp,thresh)[0][0]
+    Gfinal = masking(G,Gblur,Gsharp,thresh)[0][0]
+    Bfinal = masking(B,Bblur,Bsharp,thresh)[0][0]
+
+    res = np.asarray([Rfinal,Gfinal,Bfinal])
+    res = np.rollaxis(np.rollaxis(res,2),2)
     return res
 
 def unsharp_polymage(frame, lib_func):
@@ -53,7 +109,7 @@ def unsharp_polymage(frame, lib_func):
 def add_unsharp_app(app_id):
     # 1. modes
     modes = [ModeType.CV2, ModeType.P_NAIVE, ModeType.P_OPT, \
-        ModeType.PIL, ModeType.NUMBA]
+        ModeType.PIL, ModeType.NUMBA, ModeType.THEANO]
     # 2. modes that need shared library
     lib_modes = [ModeType.P_NAIVE, ModeType.P_OPT]
 
@@ -64,6 +120,7 @@ def add_unsharp_app(app_id):
     app_func_map[ModeType.P_OPT] = unsharp_polymage
     app_func_map[ModeType.PIL] = unsharp_pil
     app_func_map[ModeType.NUMBA] = unsharp_numba_driver
+    app_func_map[ModeType.THEANO] = unsharp_theano
 
     # 4. create an App object
     app_dir = os.path.dirname(os.path.realpath(__file__))
