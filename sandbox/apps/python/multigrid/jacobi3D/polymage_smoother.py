8184ae011c9928ed28f4617aa34f30cc37aa03a7
kumudhan <kumsbpips@gmail.com>
2016-07-21 19:31:05 +0530

    WIP: updated jacobi3D to use TStencil. Still breaks at bounds_check due to the absence of support for ghost zones
    * Tstencil is now a Function, and not an object.
    * TODO: implement proper ghost zones by inferring from the origin position and the
            size of the kernel

--- sandbox/apps/python/multigrid/jacobi3D/polymage_smoother.py
+++ sandbox/apps/python/multigrid/jacobi3D/polymage_smoother.py
@@ -7,14 +7,14 @@ sys.path.insert(0, ROOT)
 from compiler   import *
 from constructs import *
 
-def w_jacobi(U_, F_, l, name, app_data):
+def w_jacobi(U_, F_, l, name, app_data, T):
     pipe_data = app_data['pipe_data']
 
     z = pipe_data['z']
     y = pipe_data['y']
     x = pipe_data['x']
 
-    L = app_data['L']
+    # L = app_data['L']
 
     invhh = pipe_data['invhh']
 
@@ -23,32 +23,35 @@ def w_jacobi(U_, F_, l, name, app_data):
 
     extent = pipe_data['extent']
     interior = pipe_data['interior']
-    ghosts = pipe_data['ghosts']
+    # ghosts = pipe_data['ghosts']
 
-    inner_box = interior[l]['inner_box']
+    # inner_box = interior[l]['inner_box']
 
-    W_ = Function(([z, y, x], [extent[l], extent[l], extent[l]]),
-                  Double, str(name))
+    coeff = c * invhh[l]
+
+    z_kernel = [[0, coeff, 0], [coeff, 1 - 6 * coeff, coeff], [0, coeff, 0]]
+    z_minus_1_kernel = [[0, 0, 0], [0, coeff, 0], [0, 0, 0]]
+    z_plus_1_kernel = [[0, 0, 0], [0, coeff, 0], [0, 0, 0]]
+
+    kernel = [z_minus_1_kernel, z_kernel, z_plus_1_kernel]
+
+    W_ = TStencil(([z, y, x], [extent[l], extent[l], extent[l]]), Double, str(name), T)
+
+    #import pudb; pudb.set_trace();
 
     if U_ != None:
-        W_.defn = [ Case(inner_box,
-                         U_(z, y, x) \
-                       - c * (( \
-                         U_(z  , y  , x  ) * 6.0 \
-                       - U_(z-1, y  , x  )       \
-                       - U_(z+1, y  , x  )       \
-                       - U_(z  , y-1, x  )       \
-                       - U_(z  , y+1, x  )       \
-                       - U_(z  , y  , x-1)       \
-                       - U_(z  , y  , x+1)       \
-                       ) * invhh[l]              \
-                       - F_(z  , y  , x  ))) ]
+        stencil = Stencil(U_, [z, y, x], kernel)
+        W_.defn = (stencil - c * F_(z, y, x))
     else:
-        W_.defn = [ Case(inner_box, c * F_(z, y, x)) ]
+        stencil_input = Function(([z, y, x], [extent[l], extent[l], extent[l]]),
+                                 Double, 'stencil_input')
+        stencil_input.defn = [0]
+        stencil = Stencil(stencil_input, [z, y, x], kernel)
+        W_.defn = (stencil - c * F_(z, y, x))
 
-    if l == L:
-        set_ghosts(W_, ghosts[l], U_(z, y, x))
-    else:
-        set_ghosts(W_, ghosts[l], 0.0)
+    # if l == L:
+    #     set_ghosts(W_, ghosts[l], U_(z, y, x))
+    # else:
+    #     set_ghosts(W_, ghosts[l], 0.0)
 
     return W_
