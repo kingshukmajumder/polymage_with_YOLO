78e8697df847e7a2842317d82fa49ffab940b0b7
kumudhan <kumsbpips@gmail.com>
2016-09-23 22:38:47 +0530

    Reverting back the polymage benchmark file. Hand changed it for testing matmul

--- sandbox/apps/python/linear_algebra/matrix_ops/polymage_matops.py
+++ sandbox/apps/python/linear_algebra/matrix_ops/polymage_matops.py
@@ -25,11 +25,14 @@ def mat_ops(pipe_data):
     col = Interval(UInt, 0, C-1)
 
     mat1 = Matrix(Float, "mat1", [R, C], [x, y])
-    #mat2 = Matrix(Float, "mat3", [R, C], [x, y])
-    '''
+    mat2 = Matrix(Float, "mat3", [R, C], [x, y])
+
+    # Multiplication
+    mul = mat1 * mat2
+    
     # Scalar multiplication
     scalar_mul = Matrix(Float, "scalar_mul", [R, C], [x, y])
-    scalar_mul.defn = [mat1(x,y) * 10]
+    scalar_mul.defn = [mul(x,y) * 10]
     # return scalar_mul
 
     # Element-wise addition
@@ -55,38 +58,5 @@ def mat_ops(pipe_data):
     # Element-wise division
     div = Matrix(Float, "div", [R, C], [x, y])
     div.defn = [transpose(x,y) / mat2(x,y)]
-    #return div
-
-    # Determinant of a matrix
-    det = Matrix.det(div)
-    # return det
-    '''
-    # Inverse of a matrix
-    inv = Matrix.inverse(mat1)
-    return inv
-
-    #Iy = Function(([x, y], [row, col]), Float, "Iy")
-    #Iy.defn = [Sin(mat1(x,y))]
-    #return Iy
-
-
-    #new = div * mat1
-    #return new
-
-    #cond = Condition(x, '>=', 0) & Condition(x, '<=', R-1) & \
-    #       Condition(y, '<=', C-1) & Condition(y, '>=', 0)
-
-    # A pipeline with completely aimless random sequence of computations
-    # to test the math functions support
-
-    # sin(image1)
-
-    #cos = Function(([x, y], [row, col]), Float, "_cos")
-    #cos.defn = [ Cos(mat1(x, y)) ]
-
-
-    #sin = Function(([x, y], [row, col]), Float, "_sin")
-    #sin.defn = [ Case(cond, Cos(mat1(x, y))) ]
-
-    #return cos
+    return div
 
