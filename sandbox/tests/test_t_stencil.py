77ce5803a07a4d3ee920dd92d8f7128de5428db9
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2016-11-20 18:14:57 +0530

    extract common code from test_t_stencil; minimized test code length

--- sandbox/tests/test_t_stencil.py
+++ sandbox/tests/test_t_stencil.py
@@ -9,21 +9,46 @@ from compiler import *
 from constructs import *
 
 
+# common
+P = Parameter(Int, "P")
+R = Parameter(Int, "R")
+C = Parameter(Int, "C")
+T = Parameter(Int, "T")
+x = Variable(Int, "x")
+y = Variable(Int, "y")
+z = Variable(Int, "z")
+
+plane = Interval(Int, 0, P-1)
+row = Interval(Int, 0, R-1)
+col = Interval(Int, 0, C-1)
+
+
+def dump_cpp_and_graph(pipeline, filename):
+
+    dot_file = filename+"_graph.dot"
+    png_file = filename+".png"
+    g = pipeline.pipeline_graph
+    g.write(dot_file)
+    dotty_str = "dot -Tpng "+dot_file+" -o "+png_file
+    subprocess.check_output(dotty_str, shell=True)
+
+    c_file_name = filename+'.cpp'
+    c_file = open(c_file_name, 'w')
+    c_file.write(pipeline.generate_code().__str__())
+    c_file.close()
+
+    return
+
+
 def test_t_stencil_1d():
-
-    R = Parameter(Int, "R")
-    T = Parameter(Int, "T")
-    x = Variable(Int, "x")
-
-    xrow = Interval(Int, 0, R-1)
 
     img = Image(Float, "input", [R])
 
-    f = Function(([x], [xrow]), Float, "f")
+    f = Function(([x], [row]), Float, "f")
     f.defn = [3.1416 * img(x)]
 
     stencil = Stencil(f, [x], [1, 2, 3])
-    tstencil = TStencil(([x], [xrow]), Float, "out", T)
+    tstencil = TStencil(([x], [row]), Float, "out", T)
     tstencil.defn = [ stencil + img(x-1) ]
 
     p_est = [ (R, 1024)]
@@ -37,31 +62,13 @@ def test_t_stencil_1d():
                              pipe_name = "tstencil_1d",
                              options = opts)
 
-    filename = "test_t_stencil_1d_graph"
-    dot_file = filename+".dot"
-    png_file = filename+".png"
-    g = pipeline.pipeline_graph
-    g.write(filename+".dot")
-    dotty_str = "dot -Tpng "+dot_file+" -o "+png_file
-    subprocess.check_output(dotty_str, shell=True)
+    filename = "test_t_stencil_1d"
 
-    filename = 'test_t_stencil_1d.cpp'
-    c_file = open(filename, 'w')
-    c_file.write(pipeline.generate_code().__str__())
-    c_file.close()
+    dump_cpp_and_graph(pipeline, filename)
 
     return
 
 def test_t_stencil_2d():
-
-    R = Parameter(Int, "R")
-    C = Parameter(Int, "C")
-    T = Parameter(Int, "T")
-    x = Variable(Int, "x")
-    y = Variable(Int, "y")
-
-    row = Interval(Int, 0, R-1)
-    col = Interval(Int, 0, C-1)
 
     img = Image(Float, "input", [R, C])
 
@@ -77,34 +84,13 @@ def test_t_stencil_2d():
                              param_estimates = p_est,
                              pipe_name = "tstencil_2d")
 
-    filename = "test_t_stencil_2d_graph"
-    dot_file = filename+".dot"
-    png_file = filename+".png"
-    g = pipeline.pipeline_graph
-    g.write(filename+".dot")
-    dotty_str = "dot -Tpng "+dot_file+" -o "+png_file
-    subprocess.check_output(dotty_str, shell=True)
+    filename = "test_t_stencil_2d"
 
-    filename = 'test_t_stencil_2d.cpp'
-    c_file = open(filename, 'w')
-    c_file.write(pipeline.generate_code().__str__())
-    c_file.close()
+    dump_cpp_and_graph(pipeline, filename)
 
     return
 
 def test_t_stencil_3d():
-
-    P = Parameter(Int, "P")
-    R = Parameter(Int, "R")
-    C = Parameter(Int, "C")
-    T = Parameter(Int, "T")
-    x = Variable(Int, "x")
-    y = Variable(Int, "y")
-    z = Variable(Int, "z")
-
-    plane = Interval(Int, 0, P-1)
-    row = Interval(Int, 0, R-1)
-    col = Interval(Int, 0, C-1)
 
     img = Image(Float, "input", [P, R, C])
 
@@ -122,17 +108,8 @@ def test_t_stencil_3d():
                              param_estimates = p_est,
                              pipe_name = "tstencil_3d")
 
-    filename = "test_t_stencil_3d_graph"
-    dot_file = filename+".dot"
-    png_file = filename+".png"
-    g = pipeline.pipeline_graph
-    g.write(filename+".dot")
-    dotty_str = "dot -Tpng "+dot_file+" -o "+png_file
-    subprocess.check_output(dotty_str, shell=True)
+    filename = "test_t_stencil_3d"
 
-    filename = 'test_t_stencil_3d.cpp'
-    c_file = open(filename, 'w')
-    c_file.write(pipeline.generate_code().__str__())
-    c_file.close()
+    dump_cpp_and_graph(pipeline, filename)
 
     return
