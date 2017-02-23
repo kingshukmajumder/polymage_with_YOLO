7c01b0ae7f7758aa0379abc2b2cfe14e9db6c7bd
Abhinav Baid <abhinavbaid@gmail.com>
2017-02-23 16:05:18 +0530

    Add low pass filtering app

---
+++ sandbox/apps/python/dsp/low_pass/polymage_low_pass.py
@@ -0,0 +1,42 @@
+from __init__ import *
+
+import sys
+import subprocess
+import numpy as np
+from fractions import Fraction
+
+sys.path.insert(0, ROOT)
+
+from compiler import *
+from constructs import *
+
+def low_pass(pipe_data):
+
+    N = Parameter(UInt, "N")
+    C = Parameter(Double, "C")
+    F = Parameter(Double, "F")
+
+    pipe_data['N'] = N
+    pipe_data['C'] = C
+    pipe_data['F'] = F
+
+    x = Variable(UInt, 'x')
+
+    sig = Wave(Double, "sig", N, x)
+    hs = sig.fft("hs")
+
+    fsran = Interval(UInt, 0, N / 2)
+    fs = Function(([x], [fsran]), Double, "fs")
+    fs.defn = [ Cast(Double, x) / N ]
+
+    hs_lp = Wave(Complex, "hs_lp", N / 2 + 1, x)
+    cond1 = Condition(fs(x), '>', C)
+    cond2 = Condition(fs(x), '<=', C)
+    hs_lp.defn = [ Case(cond1, hs(x) * F), \
+                   Case(cond2, hs(x)) ]
+    scaled_sig = hs_lp.ifft("scaled_sig", N)
+
+    out_sig = Wave(Double, "out_sig", N, x)
+    out_sig.defn = [ scaled_sig(x) / N ]
+
+    return out_sig
---
+++ sandbox/apps/python/dsp/low_pass/printer.py
@@ -0,0 +1,32 @@
+import sys
+
+def print_line(to_file=None):
+    if to_file:
+        print("--------------------------------------------------", file=to_file)
+    else:
+        print("--------------------------------------------------")
+
+def print_header():
+    print("Signal Low Pass Filtering")
+
+def print_usage():
+    print("[main]: Usage: ")
+    print("[main]: "+sys.argv[0]+" <mode> <image> ", end=" ")
+    print("<rows> <cols> <nruns>")
+    print("[main]: <mode>  :: {'new', 'existing', 'tune'}")
+
+def print_config(app_data):
+    app_args = app_data['app_args']
+    length = app_data['length']
+    cutoff = app_data['cutoff']
+    factor = app_data['factor']
+    print_line()
+    print("# Problem Settings #")
+    print("")
+    print("[main]: mode        =", app_args.mode)
+    #print("[main]: image       =", app_args.img_file)
+    print("[main]: signal length  =", length)
+    print("[main]: cutoff frequency in Hz =", cutoff)
+    print("[main]: attenuation factor =", factor)
+    print("[main]: nruns       =", app_args.runs)
+    print_line()
