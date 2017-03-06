387edb64845ab8139f7f40ca22a56905529862c1
Abhinav Baid <abhinavbaid@gmail.com>
2017-03-06 15:57:57 +0530

    Add FIR filter frequency response app

---
+++ sandbox/apps/python/dsp/freqz/exec_pipe.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import ctypes
+import numpy as np
+import time
+
+from printer import print_line
+
+from compiler   import *
+from constructs import *
+from utils import *
+
+def call_pipe(app_data):
+    length = app_data['length']
+
+    sig_data = app_data['sig_data']
+    IN = sig_data['IN']
+    OUT1 = sig_data['OUT1']
+    OUT2 = sig_data['OUT2']
+
+    # lib function name
+    func_name = 'pipeline_'+app_data['app']
+    pipe_func = app_data[func_name]
+
+    # lib function args
+    pipe_args = []
+    pipe_args += [ctypes.c_uint(length)]
+    pipe_args += [ctypes.c_void_p(IN.ctypes.data)]
+    pipe_args += [ctypes.c_void_p(OUT1.ctypes.data)]
+    pipe_args += [ctypes.c_void_p(OUT2.ctypes.data)]
+
+    # call lib function
+    pipe_func(*pipe_args)
+    return
+
+def freqz(app_data):
+    it  = 0
+    app_args = app_data['app_args']
+   
+    runs = int(app_args.runs)
+    timer = app_args.timer
+    if timer == True:
+        t1 = time.time()
+
+    while it < runs :
+        call_pipe(app_data)
+        it += 1
+
+    print('OUTPUT: w')
+    print(app_data['sig_data']['OUT2'])
+
+    print('OUTPUT: h')
+    print(app_data['sig_data']['OUT1'])
+
+    print('IN')
+    print(app_data['sig_data']['IN'])
+
+    if timer == True:
+        t2 = time.time()
+
+        time_taken = float(t2) - float(t1)
+        print("")
+        print("[exec_pipe] : time taken to execute = ", (time_taken * 1000) / runs, " ms")
+
+    return
+
---
+++ sandbox/apps/python/dsp/freqz/polymage_freqz.py
@@ -0,0 +1,37 @@
+from __init__ import *
+
+import sys
+import subprocess
+import numpy as np
+from fractions import Fraction
+
+sys.path.insert(0, ROOT)
+
+from compiler import *
+from constructs import *
+
+def freqz(pipe_data):
+
+    N = Parameter(UInt, "N")
+
+    pipe_data['N'] = N
+
+    x = Variable(UInt, 'x')
+    y = Variable(Int, 'y')
+    z = Variable(Int, 'z')
+
+    b = Wave(Double, "b", N)
+
+    w = Wave(Double, "w", 512, x)
+    w.defn = [ Pi() / 512 * x ]
+
+    freq_int = Interval(Int, 0, 511)
+    coeff_int = Interval(Int, 0, N-1)
+
+    h = Reduction(([z], [freq_int]), ([z, y], [freq_int, coeff_int]), \
+                    Complex, "h")
+    h.defn = [ Reduce(h(z), \
+                      b(y) * Exp(Cast(Complex, -1.0j * w(z) * y)), \
+                      Op.Sum) ]
+
+    return w, h
