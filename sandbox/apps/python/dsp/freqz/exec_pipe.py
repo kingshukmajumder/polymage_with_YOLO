410692ce2cdd619ec5386dec8a7186963b8a6a45
Abhinav Baid <abhinavbaid@gmail.com>
2017-03-06 16:33:02 +0530

    Add frequency response plotting to the app

--- sandbox/apps/python/dsp/freqz/exec_pipe.py
+++ sandbox/apps/python/dsp/freqz/exec_pipe.py
@@ -3,6 +3,8 @@ import os
 import ctypes
 import numpy as np
 import time
+from scipy import signal
+import matplotlib.pyplot as plt
 
 from printer import print_line
 
@@ -61,6 +63,26 @@ def freqz(app_data):
         time_taken = float(t2) - float(t1)
         print("")
         print("[exec_pipe] : time taken to execute = ", (time_taken * 1000) / runs, " ms")
+
+    print()
+    print("Plotting frequency response of 2 FIR filters...")
+    print("Blue: low pass with cutoff frequency = 0.5 Hz")
+    print("Red: band stop with low cutoff frequency = 0.3 Hz; high cutoff frequency = 0.8 Hz")
+
+    w1, h1 = app_data['sig_data']['OUT2'].copy(), app_data['sig_data']['OUT1'].copy()
+    app_data['sig_data']['IN'] = signal.firwin(41, [0.3, 0.8])
+    call_pipe(app_data)
+    w2, h2 = app_data['sig_data']['OUT2'], app_data['sig_data']['OUT1']
+
+    assert len(w1) == len(w2) and all([i == j for i, j in zip(w1, w2)])
+
+    plt.title('Digital filter frequency response')
+    plt.plot(w1, 20*np.log10(np.abs(h1)), 'b')
+    plt.plot(w2, 20*np.log10(np.abs(h2)), 'r')
+    plt.ylabel('Amplitude Response (dB)')
+    plt.xlabel('Frequency (rad/sample)')
+    plt.grid()
+    plt.show()
 
     return
 
