27eff4f6a4ced85734de2660918f16b78a7f8147
Abhinav Baid <abhinavbaid@gmail.com>
2017-03-24 12:13:17 +0530

    Add comments to explain what the unwanted_spectral app is doing

--- sandbox/apps/python/dsp/unwanted_spectral/polymage_unwanted_spectral.py
+++ sandbox/apps/python/dsp/unwanted_spectral/polymage_unwanted_spectral.py
@@ -13,7 +13,7 @@ from constructs import *
 def unwanted_spectral(pipe_data):
 
     N = Parameter(UInt, "N")
-    Fs = Parameter(Double, "F")
+    Fs = Parameter(Double, "F") # Sample rate
 
     pipe_data['N'] = N
     pipe_data['F'] = Fs
@@ -23,34 +23,44 @@ def unwanted_spectral(pipe_data):
     y = Wave(Double, "sig", N)
 
     nyq_rate = Fs / 2.0
-    ripple_db = 95.0
+    ripple_db = 95.0 # Stopband attenuation in dB
     ntaps = 670
     beta = Wave.kaiser_beta(ripple_db)
 
-    cutoff_hz = 1000
+    cutoff_hz = 1000 # Passband frequency in Hz
+
+    # Design the filter
     taps1 = Wave.firwin(ntaps, cutoff_hz / nyq_rate, "taps1", window=('kaiser', beta))
+
+    # Filter the data and compensate for delay
     filtered_x = y.lfilter_fir(taps1, "filtered_sig")
 
     ylp = Wave(Double, "ylp", N - ntaps + 1, x)
     ylp.defn = [ filtered_x(x + ntaps - 1) ]
 
+    # Downsample the lowpass filtered signal by a factor of 10
     Fd = Fs//10
     yds = ylp.downsample(10, "yds")
 
     nyq_rate = Fd / 2.0
-    ripple_db = 60.0
+    ripple_db = 60.0 # Stopband attenuation in dB
     ntaps = 3998
     beta = Wave.kaiser_beta(ripple_db)
 
-    cutoff_hz1 = 55
-    cutoff_hz2 = 65
+    cutoff_hz1 = 55 # Passband frequency 1 in Hz
+    cutoff_hz2 = 65 # Passband frequency 2 in Hz
+
+    # Design the filter
     taps2 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps2", window=('kaiser', beta))
+
+    # Filter the data and compensate for delay
     filtered_x1 = yds.lfilter_fir(taps2, "filtered_down_sig")
 
     M = yds.length
     ybs = Wave(Double, "ybs", M - ntaps, x)
     ybs.defn = [ filtered_x1(x + ntaps) ]
 
+    # Upsample the signal to bring it back to the original sample rate
     yf = ybs.interp_fft(10, "yf")
 
     return yf
