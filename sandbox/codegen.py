7b3d976e3e2e3c07ba6eed775202d906fef76cbb
Abhinav Baid <abhinavbaid@gmail.com>
2017-04-20 17:50:41 +0530

    Put the ivdep pragma on the reduction (inner) loop as icpc can vectorize it

--- sandbox/codegen.py
+++ sandbox/codegen.py
@@ -626,7 +626,7 @@ def generate_c_naive_from_isl_ast(pipe, polyrep, node, body, cparam_map,
                     omp_par_str = "omp parallel for schedule(static)"
                     omp_pragma = genc.CPragma(omp_par_str)
                     body.add(omp_pragma)
-                if node == red_nest[n_rloops-2] and big_part:
+                if node == red_nest[n_rloops-1] and big_part:
                     vec_pragma = genc.CPragma("ivdep")
                     body.add(vec_pragma)
             elif n_rloops == 1:
