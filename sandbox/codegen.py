e254f91bb621f958909bbcc599dc9c6f870ac46e
Abhinav Baid <abhinavbaid@gmail.com>
2017-05-04 17:50:09 +0530

    Change unroll-jam factor to 4

--- sandbox/codegen.py
+++ sandbox/codegen.py
@@ -633,7 +633,7 @@ def generate_c_naive_from_isl_ast(pipe, polyrep, node, body, cparam_map,
                     omp_pragma = genc.CPragma(omp_par_str)
                     body.add(omp_pragma)
                 if node == red_nest[n_rloops-2] and big_part:
-                    ujam_pragma = genc.CPragma("unroll_and_jam (16)")
+                    ujam_pragma = genc.CPragma("unroll_and_jam (4)")
                     body.add(ujam_pragma)
                 if node == red_nest[n_rloops-1] and big_part:
                     vec_pragma = genc.CPragma("ivdep")
