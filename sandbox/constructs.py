7e594fedf11355a2d2734cf40286844e4ada9905
Abhinav Baid <abhinavbaid@gmail.com>
2017-05-11 17:13:46 +0530

    Allow design of unscaled filters and use exact value of middle in firwin

--- sandbox/constructs.py
+++ sandbox/constructs.py
@@ -2891,7 +2891,7 @@ class Wave(Function):
                     Exp(x) * ChbEvl(32.0 / x - 2.0, 1) / Sqrt(x))
 
     @classmethod
-    def firwin(cls, N, cutoff, out_name, window='hamming', pass_zero=True):
+    def firwin(cls, N, cutoff, out_name, window='hamming', pass_zero=True, div_by_sum=True):
         """FIR filter design using the window method.
 
         :param N: length of the filter (number of coefficients).
@@ -2918,6 +2918,7 @@ class Wave(Function):
         scaled_coeffs = Wave(Double, sc_name, N, out_var)
         middle = (N - 1) // 2
         cond1 = Condition(out_var, '==', middle)
+        middle = (N - 1) / 2
 
         scalar_int = Interval(UInt, 0, 0)
         coeffs_int = Interval(UInt, 0, N-1)
@@ -2980,7 +2981,7 @@ class Wave(Function):
 
         fir_coeffs = Wave(out_typ, out_name, N, out_var)
         fir_coeffs.defn = [ scaled_coeffs(out_var) / coeffs_sum(0) ]
-        return fir_coeffs
+        return fir_coeffs if div_by_sum else scaled_coeffs
 
     @classmethod
     def kaiserord(cls, ripple, width):
