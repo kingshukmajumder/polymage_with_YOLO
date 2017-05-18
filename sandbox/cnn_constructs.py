259da01562367732b8df11b8b2385d69b46c2c22
kumudhan <kumsbpips@gmail.com>
2017-05-18 19:22:32 +0530

    Initial Commit for RELU construct

--- sandbox/cnn_constructs.py
+++ sandbox/cnn_constructs.py
@@ -31,7 +31,7 @@ class Layer(object):
         return bwd_func
 
 class DataLayer(Layer):
-    def __init__(self, in_w, in_h, in_ch, num_sample, data):
+    def __init__(self, in_w, in_h, in_ch, num_sample, data, typ):
         self._data = data
         self._in_w = in_w
         self._in_h = in_h
@@ -39,6 +39,7 @@ class DataLayer(Layer):
         self._num_sample = num_sample
         self._out_dims = 4
         self._fwd_function = data
+        self._typ = typ
         Layer.__init__(0)
 
     @property
@@ -56,6 +57,9 @@ class DataLayer(Layer):
     @property
     def data(self):
         return self._data
+    @property
+    def typ(self):
+        return self._typ
     @property
     def out_dims(self):
         return self._out_dims
@@ -79,4 +83,97 @@ class DataLayer(Layer):
         # Simply returning
         return
 
-   
+class ReLULayer(Layer):
+    def __init__(self, in_layer):
+        assert(in_layer)
+        self._in_f = in_layer.forward
+        dim = in_layer.out_dims
+        Layer.__init__(in_layer)
+        if dim == 1:
+            x = Variable(UInt, 'x')
+            X = Interval(UInt, 0, out_dim_size(0)) 
+            forward = Function(([x],[X]), Float, "forward")
+            forward.defn = [max(0, in_forward(x))]
+        elif dim == 2:
+            x = Variable(UInt, 'x')
+            X = Interval(UInt, 0, out_dim_size(0)) 
+            y = Variable(UInt, 'y')
+            Y = Interval(UInt, 0, out_dim_size(1)) 
+            forward = Function(([x, y], [X, Y]), Float, "forward")
+            forward.defn = [max(0, in_forward(x, y))]
+        elif dim == 3:
+            x = Variable(UInt, 'x')
+            X = Interval(UInt, 0, out_dim_size(0)) 
+            y = Variable(UInt, 'y')
+            Y = Interval(UInt, 0, out_dim_size(1)) 
+            z = Variable(UInt, 'z')
+            Z = Interval(UInt, 0, out_dim_size(2)) 
+            forward = Function(([x, y, z], [X, Y, Z]), Float, "forward")
+            forward.defn = [max(0, in_forward(x, y, z))]
+        elif dim == 4:
+            x = Variable(UInt, 'x')
+            X = Interval(UInt, 0, out_dim_size(0)) 
+            y = Variable(UInt, 'y')
+            Y = Interval(UInt, 0, out_dim_size(1)) 
+            z = Variable(UInt, 'z')
+            Z = Interval(UInt, 0, out_dim_size(2)) 
+            w = Variable(UInt, 'w')
+            W = Interval(UInt, 0, out_dim_size(3)) 
+            forward = Function(([x, y, z, w], [X, Y, Z, W]), Float, "forward")
+            forward.defn = [max(0, in_forward(x, y, z, w))]
+        else:
+            assert(0)
+
+    @property
+    def out_dims(self):
+        return self._in_layer.out_dims
+    @property
+    def out_dim_size(self):
+        return self._in_layer.out_dim_size
+    @property
+    def in_forward(self):
+        return self._in_f
+    @property
+    def in_
+
+    def back_propagate(self, bwd_func):
+        assert(bwd_func)
+        if not f_in_grad.defn == None:
+            dim = self._in_layer.out_dims
+            if dim == 1:
+                x = Variable(UInt, 'x')
+                X = Interval(UInt, 0, out_dim_size(0)) 
+                f_in_grad = Function(([x],[X]), Float, "f_in_grad")
+                f_in_grad.defn = bwd_func(x) * Select(Condition(in_f(x) > 0), 1, 0)
+            elif dim == 2:
+                x = Variable(UInt, 'x')
+                X = Interval(UInt, 0, out_dim_size(0)) 
+                y = Variable(UInt, 'y')
+                Y = Interval(UInt, 0, out_dim_size(1)) 
+                f_in_grad = Function(([x, y], [X, Y]), Float, "f_in_grad")
+                f_in_grad.defn = bwd_func(x, y) * Select(Condition(in_f(x, y) > 0), 1, 0)
+            elif dim == 3:
+                x = Variable(UInt, 'x')
+                X = Interval(UInt, 0, out_dim_size(0)) 
+                y = Variable(UInt, 'y')
+                Y = Interval(UInt, 0, out_dim_size(1)) 
+                z = Variable(UInt, 'z')
+                Z = Interval(UInt, 0, out_dim_size(2)) 
+                f_in_grad = Function(([x, y, z], [X, Y, Z]), Float, "f_in_grad")
+                f_in_grad.defn = bwd_func(x, y, z) * Select(Condition(in_f(x, y, z) > 0), 1, 0)
+            elif dim == 4:
+                x = Variable(UInt, 'x')
+                X = Interval(UInt, 0, out_dim_size(0)) 
+                y = Variable(UInt, 'y')
+                Y = Interval(UInt, 0, out_dim_size(1)) 
+                z = Variable(UInt, 'z')
+                Z = Interval(UInt, 0, out_dim_size(2)) 
+                w = Variable(UInt, 'w')
+                W = Interval(UInt, 0, out_dim_size(3)) 
+                f_in_grad = Function(([x, y, z, w], [X, Y, Z, W]), Float, "f_in_grad")
+                f_in_grad.defn = bwd_func(x, y, z, w) * Select(Condition(in_f(x, y, z, w) > 0), 1, 0)
+            else:
+                assert(0)
+
+    def out_dim_size(i):
+        return in_layer->out_dim_size(i)
