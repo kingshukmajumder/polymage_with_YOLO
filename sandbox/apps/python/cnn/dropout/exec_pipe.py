1589501f836d312582e5471c2940856094c08aa0
kumudhan <kumsbpips@gmail.com>
2017-05-23 19:29:34 +0530

    Adding Dropout computation

---
+++ sandbox/apps/python/cnn/dropout/exec_pipe.py
@@ -0,0 +1,62 @@
+import sys
+import os
+import ctypes
+import numpy as np
+import time
+
+from printer import print_line
+
+from compiler   import *
+from constructs import *
+from utils import *
+
+def call_pipe(app_data):
+    C = app_data['C']
+    Y = app_data['Y']
+    X = app_data['X']
+
+    img_data = app_data['img_data']
+    IN = img_data['IN']
+    OUT = img_data['OUT']
+
+    # lib function name
+    func_name = 'pipeline_'+app_data['app']
+    pipe_func = app_data[func_name]
+
+    # lib function args
+    pipe_args = []
+    pipe_args += [ctypes.c_int(C)]
+    pipe_args += [ctypes.c_int(X)]
+    pipe_args += [ctypes.c_int(Y)]
+    pipe_args += [ctypes.c_void_p(IN.ctypes.data)]
+    pipe_args += [ctypes.c_void_p(OUT.ctypes.data)]
+
+    # call lib function
+    pipe_func(*pipe_args)
+    
+    return
+
+def dropout(app_data):
+    it  = 0
+    app_args = app_data['app_args']
+   
+    runs = int(app_args.runs)
+    timer = app_args.timer
+    if timer == True:
+        t1 = time.time()
+
+    while it < runs :
+        call_pipe(app_data)
+        it += 1
+
+    print('OUTPUT1')
+    print(app_data['img_data']['OUT'])
+
+    if timer == True:
+        t2 = time.time()
+
+        time_taken = float(t2) - float(t1)
+        print("")
+        print("[exec_pipe] : time taken to execute = ", (time_taken * 1000) / runs, " ms")
+    return
+
---
+++ sandbox/apps/python/cnn/dropout/polymage_dropout.py
@@ -0,0 +1,39 @@
+from __init__ import *
+
+import sys
+import subprocess
+import numpy as np
+from fractions import Fraction
+import random
+
+sys.path.insert(0, ROOT)
+
+from compiler import *
+from constructs import *
+from cnn_constructs import *
+
+def polymage_dropout(pipe_data):
+
+    C = Parameter(UInt, "C")
+    Y = Parameter(UInt, "Y")
+    X = Parameter(UInt, "X")
+
+    c = Variable(UInt, 'c')
+    x = Variable(UInt, 'x')
+    y = Variable(UInt, 'y')
+
+    Ci = Interval(UInt, 0, C-1)
+    Yi = Interval(UInt, 0, Y-1)
+    Xi = Interval(UInt, 0, X-1)
+
+    thresh = 0.15
+    scale = 1.0/(1.0 - thresh) 
+    
+    input_mat = Matrix(Float, "input", [X, Y, C], [x, y, c])
+    mask = Matrix(Float, "mask", [X, Y, C], [x, y, c])
+    output = Matrix(Float, "output", [X, Y, C], [x, y, c])
+
+    mask.defn = [Select(Condition(random.random(), ">", thresh), scale, 0.0)]
+    output.defn = [mask(x, y, c) * input_mat(x, y, c)]
+
+    return output
