b1876b2131577e1da85c25998e5a564164e3cc21
Abhinav Baid <abhinavbaid@gmail.com>
2017-08-04 20:15:39 +0530

    Improve vuvuzela app for new inputs by adding Quint frequencies and increasing band filter widths

--- sandbox/apps/python/dsp/vuvuzela/polymage_vuvu.py
+++ sandbox/apps/python/dsp/vuvuzela/polymage_vuvu.py
@@ -53,8 +53,8 @@ def vuvu(pipe_data):
     # Filter the data and compensate for delay
     yf1 = yds.lfilter_fir_and_delay(taps1, "yf1")
 
-    cutoff_hz1 = 445 # Passband frequency 1 in Hz
-    cutoff_hz2 = 485 # Passband frequency 2 in Hz
+    cutoff_hz1 = 425 # Passband frequency 1 in Hz
+    cutoff_hz2 = 505 # Passband frequency 2 in Hz
 
     # Design the filter
     taps2 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps2", window=win)
@@ -62,23 +62,41 @@ def vuvu(pipe_data):
     # Filter the data and compensate for delay
     yf2 = yf1.lfilter_fir_and_delay(taps2, "yf2")
 
-    cutoff_hz1 = 910 # Passband frequency 1 in Hz
-    cutoff_hz2 = 950 # Passband frequency 2 in Hz
+    cutoff_hz1 = 650 # Passband frequency 1 in Hz
+    cutoff_hz2 = 750 # Passband frequency 2 in Hz
+
+    # Design the filter
+    taps2_5 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps2_5", window=win)
+
+    # Filter the data and compensate for delay
+    yf2_5 = yf2.lfilter_fir_and_delay(taps2_5, "yf2_5")
+
+    cutoff_hz1 = 870 # Passband frequency 1 in Hz
+    cutoff_hz2 = 990 # Passband frequency 2 in Hz
 
     # Design the filter
     taps3 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps3", window=win)
 
     # Filter the data and compensate for delay
-    yf3 = yf2.lfilter_fir_and_delay(taps3, "yf3")
+    yf3 = yf2_5.lfilter_fir_and_delay(taps3, "yf3")
 
-    cutoff_hz1 = 1840 # Passband frequency 1 in Hz
-    cutoff_hz2 = 1880 # Passband frequency 2 in Hz
+    cutoff_hz1 = 1325 # Passband frequency 1 in Hz
+    cutoff_hz2 = 1465 # Passband frequency 2 in Hz
+
+    # Design the filter
+    taps3_5 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps3_5", window=win)
+
+    # Filter the data and compensate for delay
+    yf3_5 = yf3.lfilter_fir_and_delay(taps3_5, "yf3_5")
+
+    cutoff_hz1 = 1780 # Passband frequency 1 in Hz
+    cutoff_hz2 = 1940 # Passband frequency 2 in Hz
 
     # Design the filter
     taps4 = Wave.firwin(ntaps+1, (cutoff_hz1 / nyq_rate, cutoff_hz2 / nyq_rate), "taps4", window=win)
 
     # Filter the data and compensate for delay
-    yf4 = yf3.lfilter_fir_and_delay(taps4, "yf4", _out_typ=Complex)
+    yf4 = yf3_5.lfilter_fir_and_delay(taps4, "yf4", _out_typ=Complex)
 
     # Upsample the signal to bring it back to the original sample rate
     yf = yf4.interp_fft(5, "yf", _out_typ=Double)
